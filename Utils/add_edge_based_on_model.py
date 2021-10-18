2b239275b80210d900298ae45a0f70d63ca477cb
jiatingyu-amss <61691158+jiatingyu-amss@users.noreply.github.com>
2021-10-18 09:08:30 +0800

    Update add_edge_based_on_model.py

--- Utils/add_edge_based_on_model.py
+++ Utils/add_edge_based_on_model.py
@@ -8,7 +8,7 @@ import networkx as nx
 ################################################################################################################
 #                     ER graph
 ################################################################################################################
-ref_net = np.loadtxt('./Simulated/ER_data/ER.txt')
+ref_net = np.loadtxt('../Example_1_Simulated/ER_data/ER.txt')
 add_edge_prop = 0.1
 add_edge_num = int(np.ceil(add_edge_prop * ref_net.sum() / 2))
 
@@ -17,7 +17,7 @@ for x in ['x1', 'x2']:
         x_cof = dataname2data(x)
         new_net_unweighted = add_edge(ref_net, x_cof, add_edge_num)
 
-        np.savetxt('./Simulated/ER_data/add_edge/' + x + '_' +
+        np.savetxt('../Example_1_Simulated/ER_data/add_edge/' + x + '_' +
                    str(loops) + '_addedge' + str(add_edge_prop) +
                    '_ER.txt', new_net_unweighted, fmt = '%d')
 
@@ -28,7 +28,7 @@ for x in ['x1', 'x2']:
 #                     BA graph
 ################################################################################################################
 # original net
-ref_net = np.loadtxt('./Simulated/BA_data/BA.txt')
+ref_net = np.loadtxt('../Example_1_Simulated/BA_data/BA.txt')
 add_edge_prop = 0.1
 add_edge_num = int(np.ceil(add_edge_prop * ref_net.sum() / 2))
 
@@ -37,7 +37,7 @@ for x in ['x1', 'x2']:
         x_cof = dataname2data(x)
         new_net_unweighted = add_edge(ref_net, x_cof, add_edge_num)
 
-        np.savetxt('./Simulated/BA_data/add_edge/' + x + '_' +
+        np.savetxt('../Example_1_Simulated/BA_data/add_edge/' + x + '_' +
                    str(loops) + '_addedge' + str(add_edge_prop) +
                    '_BA.txt', new_net_unweighted, fmt = '%d')
 
@@ -49,7 +49,7 @@ for x in ['x1', 'x2']:
 #                     lesmis graph
 ################################################################################################################
 # original net
-ref_net = np.loadtxt('./Simulated/lesmis_data/lesmis.txt')
+ref_net = np.loadtxt('../Example_1_Simulated/lesmis_data/lesmis.txt')
 add_edge_prop = 0.1
 add_edge_num = int(np.ceil(add_edge_prop * ref_net.sum() / 2))
 
@@ -58,7 +58,7 @@ for x in ['x1', 'x2']:
         x_cof = dataname2data(x)
         new_net_unweighted = add_edge(ref_net, x_cof, add_edge_num)
 
-        np.savetxt('./Simulated/lesmis_data/add_edge/' + x + '_' +
+        np.savetxt('../Example_1_Simulated/lesmis_data/add_edge/' + x + '_' +
                    str(loops) + '_addedge' + str(add_edge_prop) +
                    '_lesmis.txt', new_net_unweighted, fmt = '%d')
 
@@ -68,7 +68,7 @@ for x in ['x1', 'x2']:
 #                     cerebral_cortex graph
 ################################################################################################################
 # original net
-G = nx.read_graphml('./Simulated/cerebral_cortex_data/rhesus_cerebral.cortex_1.graphml')
+G = nx.read_graphml('../Example_1_Simulated/cerebral_cortex_data/rhesus_cerebral.cortex_1.graphml')
 ref_net = np.array(nx.adjacency_matrix(G).todense())
 ref_net = ref_net + ref_net.T
 ref_net = (ref_net > 0) * 1
@@ -81,6 +81,6 @@ for x in ['x1', 'x2']:
         x_cof = dataname2data(x)
         new_net_unweighted = add_edge(ref_net, x_cof, add_edge_num)
 
-        np.savetxt('./Simulated/cerebral_cortex_data/add_edge/' + x + '_' +
+        np.savetxt('../Example_1_Simulated/cerebral_cortex_data/add_edge/' + x + '_' +
                    str(loops) + '_addedge' + str(add_edge_prop) +
                    '_cerebral_cortex.txt', new_net_unweighted, fmt = '%d')
